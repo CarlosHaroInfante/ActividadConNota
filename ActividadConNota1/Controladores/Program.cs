8c6455b06fbc85b7962a9e322b6b348c61ac65aa
CarlosHaroInfante <145343573+CarlosHaroInfante@users.noreply.github.com>
2023-10-31 14:43:35 +0100

    Add files via upload

---
+++ ActividadConNota1/Controladores/Program.cs
@@ -0,0 +1,42 @@
+﻿using ActividadConNota1.Servicios;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        NumeroInterfaz num = new NumeroImplementacion();
+        int a = num.Mes();
+        int b = num.año();
+
+       bool cerrarMenu = false;
+
+        MenuInterfaz menu = new MenuImplementacion();
+        int c = menu.menu();
+        //int opcionElegida;
+
+        while (!cerrarMenu)
+        {
+            c = menu.menu();
+            Console.WriteLine(c);
+            
+
+            switch (c)
+            {
+                case 0:
+                    Console.WriteLine("[INFO] - Se cerrará el programa");
+                    cerrarMenu = true;
+                    break;
+                case 1:
+                    Console.WriteLine("[INFO] - Volver ha realizar");
+                    cerrarMenu = false;
+                    Console.WriteLine(a);
+                    Console.WriteLine(b);
+                    break; 
+                default:
+                    Console.WriteLine("No ha elegido una opción valida");
+                    break;
+            }
+        }
+    
+    }
+}
---
+++ ActividadConNota1/Servicios/MenuImplementacion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActividadConNota1.Servicios
+{
+    internal class MenuImplementacion : MenuInterfaz
+    {
+        public int menu()
+        {
+            int opcionElegida;
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine("Quiere volver a hacerlo");
+            Console.WriteLine("[0] - N");
+            Console.WriteLine("[1] - S");
+            Console.WriteLine("-------------------------------");
+            opcionElegida = Console.ReadKey(true).KeyChar - ('0');
+            return opcionElegida;
+        }
+    }
+}
